1b7eaa755a75bda234a3b6a891c4acb41d789f0f
Murphy <Ryan.P.Murphy@emc.com>
2017-01-05 15:29:29 -0500

    clean up code

--- generateSummary.rb
+++ generateSummary.rb
@@ -11,159 +11,41 @@ include ReportEngine
 #Generate Variables Used by multiple clusters
 Process.setproctitle("XtremIO Summary Report")
 location = DossierEngine.getFileLocation
-dossierCount = getDossierCount(location)
-multipleJsonArray = unpackMultipleDossierJson(location)
+dossierCount = DossierEngine.getDossierCount(location)
+multipleJsonArray = DossierEngine.unpackMultipleDossierJson(location)
 
 #Main Program Loop
 clustersArray = []
-totalPhysUsable = 0.0
-totalPhysConsumed = 0.0
-totalPhysFree = 0.0
-totalSvgConsumed = 0.0
-totalSvgTotalLogical = 0.0
-totalSourceVolCount = 0
-totalSnapVolCount = 0
-totalDRR = 0
-totalSourceLogicalConsumed = 0.0
-totalSnapLogicalConsumed = 0.0
-totalCombinedLogicalConsumed = 0.0
-totalSourceDRR = 0.0
-totalSnapDRR = 0.0
-totalCombinedDRR = 0.0
 dossierCount = 1
 
 #for each dossier file
 multipleJsonArray.each do |jsonHash|
   clusterCount = DossierEngine.getClusterCount(jsonHash)
-  counter = 0
   allVolumes = DossierEngine.getAllVolumes(jsonHash)
   allSnapshotGroups = DossierEngine.getAllSnapshotGroups(jsonHash)
+  counter = 0
 
-  puts "[Status] - Generating XtremIO Summary from dossier #{dossierCount}"
+  puts "[Status] - Starting XtremIO analysis for dossier #{dossierCount}"
+  puts "[Status] - Dossier #{dossierCount} has #{clusterCount} XtremIO cluster(s) attached"
+
   #for each cluster in the dossier file
-  puts jsonHash["AllSystems"].length
   clusterCount.times do
-    clusterCode = jsonHash["SystemsInfo"][counter]["sys_sw_version"]
-    if clusterCode.include? '3.0.'
-      pstn = jsonHash["SystemsInfo"][counter]["psnt"]
-      puts "[Warning] - Skipping #{pstn} because it is a 3.x cluster"
-    else
-      clusterSerial = jsonHash["SystemsInfo"][counter]["psnt"]
-      puts "[Status] - Starting #{clusterSerial} analysis"
-      clusterName = jsonHash["Systems"][counter]["name"]
-      clusterType = jsonHash["Systems"][counter]["size_and_capacity"]
-      clusterState = jsonHash["AllSystems"][counter]["sys_health_state"]
-      clusterPhysConsumed = (((jsonHash["Systems"][counter]["ud_ssd_space_in_use"]).to_f)/1024.0/1024.0/1024.0).round(2)
-      clusterPhysFree = (((jsonHash["AllSystems"][counter]["free_ud_ssd_space"]).to_f)/1024.0/1024.0/1024.0).round(2)
-      clusterPhysUsable = (((jsonHash["Systems"][counter]["ud_ssd_space"]).to_f)/1024.0/1024.0/1024.0).round(2)
-      clusterPercFull = ((clusterPhysConsumed / clusterPhysUsable ) * 100).round(2)
-      clusterSvgCount = 0
-      clusterSvgConsumed = 0.0
-      clusterSvgTotalLogical = 0.0
-      clusterSourceVolCount = 0
-      clusterSnapVolCount = 0
-      clusterDedupe = jsonHash["AllSystems"][counter]["dedup_ratio"].round(2)
-      clusterCompression = jsonHash["AllSystems"][counter]["compression_factor"].round(2)
-      clusterDRR = jsonHash["AllSystems"][counter]["data_reduction_ratio"].round(2)
-      clusterThinRatio = jsonHash["AllSystems"][counter]["thin_provisioning_ratio"].round(2)
-      clusterOverallEff = jsonHash["AllSystems"][counter]["overall_efficiency_ratio"].round(2)
-      clusterSourceVolLogicalConsumed = 0.0
-      clusterSourceVolTotalLogical = 0.0
-      clusterSnapVolLogicalConsumed = 0.0
-      clusterSnapVolTotalLogical = 0.0
-
-      #generate logical consumed and total logical
-      allSnapshotGroups.each do |sg|
-        if sg["sys_id"][1] == jsonHash["Systems"][counter]["name"]
-          clusterSvgCount += 1
-          clusterSvgConsumed += ((sg["logical_space_in_use"].to_f)/1024.0/1024.0/1024.0).round(2)
-          clusterSvgTotalLogical += ((sg["vol_size"].to_f)/1024.0/1024.0/1024.0).round(2)
-        end
-      end
-
-      #generate source and snap count
-      allVolumes.each do |vol|
-        if vol["sys_id"][1] == jsonHash["Systems"][counter]["name"]
-          if vol["created_from_volume"] == ""
-            clusterSourceVolCount += 1
-            clusterSourceVolLogicalConsumed += ((vol["logical_space_in_use"].to_f)/1024.0/1024.0/1024.0).round(2)
-            clusterSourceVolTotalLogical += ((vol["vol_size"].to_f)/1024.0/1024.0/1024.0).round(2)
-            totalSourceLogicalConsumed += ((vol["logical_space_in_use"].to_f)/1024.0/1024.0/1024.0).round(2)
-
-          else
-            clusterSnapVolCount += 1
-            clusterSnapVolLogicalConsumed += ((vol["logical_space_in_use"].to_f)/1024.0/1024.0/1024.0).round(2)
-            clusterSnapVolTotalLogical += ((vol["vol_size"].to_f)/1024.0/1024.0/1024.0).round(2)
-            totalSnapLogicalConsumed += ((vol["logical_space_in_use"].to_f)/1024.0/1024.0/1024.0).round(2)
-          end
-        end
-      end
-
-      #add to totals
-      totalPhysUsable += clusterPhysUsable
-      totalPhysConsumed += clusterPhysConsumed
-      totalPhysFree += clusterPhysFree
-      totalSvgConsumed += clusterSvgConsumed
-      totalSvgTotalLogical += clusterSvgTotalLogical
-      totalSourceVolCount += clusterSourceVolCount
-      totalSnapVolCount += clusterSnapVolCount
-
-
-      #verbose
-      combinedLogicalConsumed = (clusterSourceVolLogicalConsumed + clusterSnapVolLogicalConsumed).round(2)
-      sourceDRR = (clusterSourceVolLogicalConsumed / clusterPhysConsumed).round(2)
-      snapDRR = (clusterSnapVolLogicalConsumed / clusterPhysConsumed).round(2)
-      combinedDRR = ((clusterSourceVolLogicalConsumed+clusterSnapVolLogicalConsumed) / clusterPhysConsumed).round(2)
-
-      clusterData = {:pstn => clusterSerial, :name => clusterName, :code => clusterCode, :type => clusterType, :state => clusterState,
-                     :physUsable => clusterPhysUsable, :physConsumed => clusterPhysConsumed, :physFree => clusterPhysFree, :percFull => clusterPercFull,
-                     :logicalConsumed => clusterSvgConsumed, :totalLogical => clusterSvgTotalLogical, :sourceVolCount => clusterSourceVolCount, :snapVolCount => clusterSnapVolCount,
-                     :dedupe => clusterDedupe, :compression => clusterCompression, :drr => clusterDRR, :thinRatio => clusterThinRatio, :overallEff => clusterOverallEff,
-                     :sourceLogicalConsumed => clusterSourceVolLogicalConsumed, :snapLogicalConsumed => clusterSnapVolLogicalConsumed, :combinedLogicalConsumed => combinedLogicalConsumed,
-                     :sourceDRR => sourceDRR, :snapDRR => snapDRR, :combinedDRR => combinedDRR}
+    clusterData = DossierEngine.getClusterData(counter,jsonHash,allVolumes,allSnapshotGroups)
+    puts "[Status] - Starting #{clusterData[:pstn]} analysis"
+    if clusterData[:code] != '3.x'
       clustersArray.push(clusterData)
-
+      puts "[Status] - Completed #{clusterData[:pstn]} analysis"
+    else
+      puts "[Warning] - Skipping #{clusterData[:pstn]} because it is a 3.x cluster"
     end
     counter += 1
-  end
+  end #end cluster count loop
+  puts "[Status] - Completed XtremIO analysis for dossier #{dossierCount}"
   dossierCount += 1
-end
+end # end dossier file multiple json loop
 
-puts "[Status] - Generating totals from all dossier reports"
-totalDRR = (totalSvgConsumed / totalPhysConsumed).round(2)
-totalCombinedLogicalConsumed = (totalSourceLogicalConsumed + totalSnapLogicalConsumed).round(2)
-totalSourceDRR = (totalSourceLogicalConsumed / totalPhysConsumed).round(2)
-totalSnapDRR = (totalSnapLogicalConsumed / totalPhysConsumed).round(2)
-totalCombinedDRR = (totalCombinedLogicalConsumed / totalPhysConsumed).round(2)
+puts "[Status] - Generating totals from all XtremIO clusters"
+totalsHash = DossierEngine.generateTotals(clustersArray)
 
-
-
-CSV.open(DossierEngine.generateCSVFilename(location), "w") do |csv|
-  csv <<["XtremIO Summary Report:", Time.now.strftime('%m/%d/%Y - %H:%M:%S')]
-  csv <<[" "]
-  csv <<[" "]
-  csv <<["Basic Cluster Information"]
-  csv <<["Serial Number", "Cluster Name", "Cluster Code", "Cluster Type", "Cluster State", "Physical Usable (TB)", "Physical Consumed (TB)", "Physical Free (TB)", "% Full",
-         "Logical Consumed (TB)", "Total Logical (TB)", "Source Vol Count", "Snap Vol Count",
-         "Dedupe", "Compression", "DRR", "Thin Ratio", "Overall Efficiency"]
-  clustersArray.each do |clusterData|
-    csv << [clusterData[:pstn], clusterData[:name], clusterData[:code], clusterData[:type], clusterData[:state], clusterData[:physUsable], clusterData[:physConsumed],
-            clusterData[:physFree], clusterData[:percFull], clusterData[:logicalConsumed], clusterData[:totalLogical], clusterData[:sourceVolCount], clusterData[:snapVolCount],
-            clusterData[:dedupe], clusterData[:compression], clusterData[:drr], clusterData[:thinRatio], clusterData[:overallEff]]
-  end
-  csv <<["Totals","-","-","-","-",totalPhysUsable,totalPhysConsumed,totalPhysFree,"-",totalSvgConsumed,totalSvgTotalLogical,totalSourceVolCount,totalSnapVolCount,
-         "-", "-", totalDRR,"-","-"]
-
-  #if verbose flag is set
-  if DossierEngine.getFlags == true
-    csv <<[" "]
-    csv <<["Verbose Cluster Information"]
-    csv <<["Serial Number", "Cluster Name", "Physical Consumed (TB)", "Source Logical Consumed (TB)", "Snapshot Logical Consumed (TB)", "Combined Logical Consumed (TB)", "DRR - Source", "DRR - Snap", "DRR - Combined"]
-    clustersArray.each do |clusterData|
-      csv << [clusterData[:pstn], clusterData[:name], clusterData[:physConsumed], clusterData[:sourceLogicalConsumed], clusterData[:snapLogicalConsumed], clusterData[:combinedLogicalConsumed],
-              clusterData[:sourceDRR], clusterData[:snapDRR], clusterData[:combinedDRR]]
-    end
-    csv <<["Totals","-",totalPhysConsumed,totalSourceLogicalConsumed,totalSnapLogicalConsumed,totalCombinedLogicalConsumed,totalSourceDRR,totalSnapDRR,totalCombinedDRR]
-  end
-
-end
+puts "[Status] - Generating the csv summary file in #{location}"
+DossierEngine.generateSummaryCSV(clustersArray,totalsHash,location)
--- modules/dossierEngine.rb
+++ modules/dossierEngine.rb
@@ -59,6 +59,154 @@ module DossierEngine
     return multipleJsonArray
   end
 
+  #
+  def getClusterData(counter,jsonHash,allVolumes,allSnapshotGroups)
+    clusterData = nil
+    clusterCode = jsonHash["SystemsInfo"][counter]["sys_sw_version"]
+    clusterSerial = jsonHash["SystemsInfo"][counter]["psnt"]
+
+    #check the cluster code level
+    if clusterCode.include? '3.0.'
+      clusterData = {:pstn => clusterSerial, :code => '3.x'}
+    else
+      clusterName = jsonHash["Systems"][counter]["name"]
+      clusterType = jsonHash["Systems"][counter]["size_and_capacity"]
+      clusterState = jsonHash["AllSystems"][counter]["sys_health_state"]
+      clusterPhysConsumed = (((jsonHash["Systems"][counter]["ud_ssd_space_in_use"]).to_f)/1024.0/1024.0/1024.0).round(2)
+      clusterPhysFree = (((jsonHash["AllSystems"][counter]["free_ud_ssd_space"]).to_f)/1024.0/1024.0/1024.0).round(2)
+      clusterPhysUsable = (((jsonHash["Systems"][counter]["ud_ssd_space"]).to_f)/1024.0/1024.0/1024.0).round(2)
+      clusterPercFull = ((clusterPhysConsumed / clusterPhysUsable ) * 100).round(2)
+      clusterSvgCount = 0
+      clusterSvgConsumed = 0.0
+      clusterSvgTotalLogical = 0.0
+      clusterSourceVolCount = 0
+      clusterSnapVolCount = 0
+      clusterDedupe = jsonHash["AllSystems"][counter]["dedup_ratio"].round(2)
+      clusterCompression = jsonHash["AllSystems"][counter]["compression_factor"].round(2)
+      clusterDRR = jsonHash["AllSystems"][counter]["data_reduction_ratio"].round(2)
+      clusterThinRatio = jsonHash["AllSystems"][counter]["thin_provisioning_ratio"].round(2)
+      clusterOverallEff = jsonHash["AllSystems"][counter]["overall_efficiency_ratio"].round(2)
+      clusterSourceVolLogicalConsumed = 0.0
+      clusterSourceVolTotalLogical = 0.0
+      clusterSnapVolLogicalConsumed = 0.0
+      clusterSnapVolTotalLogical = 0.0
+
+      #generate logical consumed and total logical
+      allSnapshotGroups.each do |sg|
+        if sg["sys_id"][1] == jsonHash["Systems"][counter]["name"]
+          clusterSvgCount += 1
+          clusterSvgConsumed += ((sg["logical_space_in_use"].to_f)/1024.0/1024.0/1024.0).round(2)
+          clusterSvgTotalLogical += ((sg["vol_size"].to_f)/1024.0/1024.0/1024.0).round(2)
+        end
+      end
+
+      #generate source and snap count
+      allVolumes.each do |vol|
+        if vol["sys_id"][1] == jsonHash["Systems"][counter]["name"]
+          if vol["created_from_volume"] == ""
+            clusterSourceVolCount += 1
+            clusterSourceVolLogicalConsumed += ((vol["logical_space_in_use"].to_f)/1024.0/1024.0/1024.0).round(2)
+            clusterSourceVolTotalLogical += ((vol["vol_size"].to_f)/1024.0/1024.0/1024.0).round(2)
+
+          else
+            clusterSnapVolCount += 1
+            clusterSnapVolLogicalConsumed += ((vol["logical_space_in_use"].to_f)/1024.0/1024.0/1024.0).round(2)
+            clusterSnapVolTotalLogical += ((vol["vol_size"].to_f)/1024.0/1024.0/1024.0).round(2)
+          end
+        end
+      end
+
+
+      #verbose
+      combinedLogicalConsumed = (clusterSourceVolLogicalConsumed + clusterSnapVolLogicalConsumed).round(2)
+      sourceDRR = (clusterSourceVolLogicalConsumed / clusterPhysConsumed).round(2)
+      snapDRR = (clusterSnapVolLogicalConsumed / clusterPhysConsumed).round(2)
+      combinedDRR = ((clusterSourceVolLogicalConsumed+clusterSnapVolLogicalConsumed) / clusterPhysConsumed).round(2)
+
+      clusterData = {:pstn => clusterSerial, :name => clusterName, :code => clusterCode, :type => clusterType, :state => clusterState,
+                     :physUsable => clusterPhysUsable, :physConsumed => clusterPhysConsumed, :physFree => clusterPhysFree, :percFull => clusterPercFull,
+                     :logicalConsumed => clusterSvgConsumed, :totalLogical => clusterSvgTotalLogical, :sourceVolCount => clusterSourceVolCount, :snapVolCount => clusterSnapVolCount,
+                     :dedupe => clusterDedupe, :compression => clusterCompression, :drr => clusterDRR, :thinRatio => clusterThinRatio, :overallEff => clusterOverallEff,
+                     :sourceLogicalConsumed => clusterSourceVolLogicalConsumed, :snapLogicalConsumed => clusterSnapVolLogicalConsumed, :combinedLogicalConsumed => combinedLogicalConsumed,
+                     :sourceDRR => sourceDRR, :snapDRR => snapDRR, :combinedDRR => combinedDRR}
+    end #end code check loop
+
+    return clusterData
+  end
+
+  #
+  def generateTotals(clustersArray)
+    totalPhysUsable = 0.0
+    totalPhysConsumed = 0.0
+    totalPhysFree = 0.0
+    totalSvgConsumed = 0.0
+    totalSvgTotalLogical = 0.0
+    totalSourceVolCount = 0
+    totalSnapVolCount = 0
+    totalDRR = 0
+    totalSourceLogicalConsumed = 0.0
+    totalSnapLogicalConsumed = 0.0
+    totalCombinedLogicalConsumed = 0.0
+    totalSourceDRR = 0.0
+    totalSnapDRR = 0.0
+    totalCombinedDRR = 0.0
+    #get data from each cluster
+    clustersArray.each do |clusterData|
+      totalPhysUsable += clusterData[:physUsable]
+      totalPhysConsumed += clusterData[:physConsumed]
+      totalPhysFree += clusterData[:physFree]
+      totalSvgConsumed += clusterData[:logicalConsumed]
+      totalSvgTotalLogical += clusterData[:totalLogical]
+      totalSourceVolCount += clusterData[:sourceVolCount]
+      totalSnapVolCount += clusterData[:snapVolCount]
+      totalSourceLogicalConsumed += clusterData[:sourceLogicalConsumed]
+      totalSnapLogicalConsumed += clusterData[:snapLogicalConsumed]
+    end
+    totalDRR = (totalSvgConsumed / totalPhysConsumed).round(2)
+    totalCombinedLogicalConsumed = (totalSourceLogicalConsumed + totalSnapLogicalConsumed).round(2)
+    totalSourceDRR = (totalSourceLogicalConsumed / totalPhysConsumed).round(2)
+    totalSnapDRR = (totalSnapLogicalConsumed / totalPhysConsumed).round(2)
+    totalCombinedDRR = (totalCombinedLogicalConsumed / totalPhysConsumed).round(2)
+
+    return {:totalPhysUsable => totalPhysUsable, :totalPhysConsumed => totalPhysConsumed, :totalPhysFree => totalPhysFree, :totalSvgConsumed => totalSvgConsumed,
+            :totalSvgTotalLogical => totalSvgTotalLogical, :totalSourceVolCount => totalSourceVolCount, :totalSnapVolCount => totalSnapVolCount,
+            :totalDRR => totalDRR, :totalSourceLogicalConsumed => totalSourceLogicalConsumed, :totalSnapLogicalConsumed => totalSnapLogicalConsumed,
+            :totalCombinedLogicalConsumed => totalCombinedLogicalConsumed, :totalSourceDRR => totalSourceDRR, :totalSnapDRR => totalSnapDRR,
+            :totalCombinedDRR => totalCombinedDRR}
+  end
+
+  #
+  def generateSummaryCSV(clustersArray,totalsHash,location)
+    CSV.open(DossierEngine.generateCSVFilename(location), "w") do |csv|
+      csv <<["XtremIO Summary Report:", Time.now.strftime('%m/%d/%Y - %H:%M:%S')]
+      csv <<[" "]
+      csv <<[" "]
+      csv <<["Basic Cluster Information"]
+      csv <<["Serial Number", "Cluster Name", "Cluster Code", "Cluster Type", "Cluster State", "Physical Usable (TB)", "Physical Consumed (TB)", "Physical Free (TB)", "% Full",
+             "Logical Consumed (TB)", "Total Logical (TB)", "Source Vol Count", "Snap Vol Count",
+             "Dedupe", "Compression", "DRR", "Thin Ratio", "Overall Efficiency"]
+      clustersArray.each do |clusterData|
+        csv << [clusterData[:pstn], clusterData[:name], clusterData[:code], clusterData[:type], clusterData[:state], clusterData[:physUsable], clusterData[:physConsumed],
+                clusterData[:physFree], clusterData[:percFull], clusterData[:logicalConsumed], clusterData[:totalLogical], clusterData[:sourceVolCount], clusterData[:snapVolCount],
+                clusterData[:dedupe], clusterData[:compression], clusterData[:drr], clusterData[:thinRatio], clusterData[:overallEff]]
+      end
+      csv <<["Totals","-","-","-","-",totalsHash[:totalPhysUsable],totalsHash[:totalPhysConsumed],totalsHash[:totalPhysFree],"-",totalsHash[:totalSvgConsumed],totalsHash[:totalSvgTotalLogical],totalsHash[:totalSourceVolCount],totalsHash[:totalSnapVolCount],
+             "-", "-", totalsHash[:totalDRR],"-","-"]
+
+      #if verbose flag is set
+      if DossierEngine.getFlags == true
+        csv <<[" "]
+        csv <<["Verbose Cluster Information"]
+        csv <<["Serial Number", "Cluster Name", "Physical Consumed (TB)", "Source Logical Consumed (TB)", "Snapshot Logical Consumed (TB)", "Combined Logical Consumed (TB)", "DRR - Source", "DRR - Snap", "DRR - Combined"]
+        clustersArray.each do |clusterData|
+          csv << [clusterData[:pstn], clusterData[:name], clusterData[:physConsumed], clusterData[:sourceLogicalConsumed], clusterData[:snapLogicalConsumed], clusterData[:combinedLogicalConsumed],
+                  clusterData[:sourceDRR], clusterData[:snapDRR], clusterData[:combinedDRR]]
+        end
+        csv <<["Totals","-",totalsHash[:totalPhysConsumed],totalsHash[:totalSourceLogicalConsumed],totalsHash[:totalSnapLogicalConsumed],totalsHash[:totalCombinedLogicalConsumed],totalsHash[:totalSourceDRR],totalsHash[:totalSnapDRR],totalsHash[:totalCombinedDRR]]
+      end
+    end
+  end
+
   #Opens dossier file and pulls out the json data
   def unpackDossieJson(location)
     %x[unzip #{location} -d temp1x2y3z4]
